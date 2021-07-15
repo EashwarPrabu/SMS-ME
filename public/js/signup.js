916166856713e6a87ef883326b15336cf05ea420
Eashwar Prabu <eashwarpc@gmail.com>
2021-07-15 21:22:51 +0530

    Final Commit

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,9 @@
-  //signup
-  document.getElementById("submit").addEventListener("click", function() {
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let pass = document.getElementById('psw').value;
-    let confirmpass = document.getElementById('psw1').value;
+//signup
+document.getElementById("submit").addEventListener("click", function () {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("psw").value;
+    let confirmpass = document.getElementById("psw1").value;
     /*if (pass === confirmpass)
     {
         const data = { name: name, password: pass, email: email };
